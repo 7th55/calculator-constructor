483f3ff74b17136251309701876265bc5e81ddc0
7th55 <7th55hh@gmail.com>
2023-03-14 00:21:17 +0400

    Add a property test for "FractionalNumber"

--- src/store/types/FractionalNumber.test.ts
+++ src/store/types/FractionalNumber.test.ts
@@ -1,4 +1,5 @@
-import { floatToFrac, FractionalNumber, fracToFloat } from './FractionalNumber';
+import fc from 'fast-check';
+import { floatToFrac, fracToFloat } from './FractionalNumber';
 
 export {};
 
@@ -20,5 +21,20 @@ describe('Conversion between "number" and "FractionalNumber"', () => {
     expect(fracToFloat(floatToFrac(123.456))).toBe(123.456);
     expect(fracToFloat(floatToFrac(-123.456))).toBe(-123.456);
     expect(fracToFloat(floatToFrac(0.00123))).toBe(0.00123);
+  });
+  test('Property: A composition of floatToFrac & fracToFloat is idempotent', () => {
+    fc.assert(
+      fc.property(
+        fc.integer(),
+        (num: number) => fracToFloat(floatToFrac(num)) === num
+      )
+    );
+    // FIXME: This requires support for complex numbers like "-1.401298464324817e-45"
+    // fc.assert(
+    //   fc.property(
+    //     fc.float(),
+    //     (num: number) => fracToFloat(floatToFrac(num)) === num
+    //   )
+    // );
   });
 });
